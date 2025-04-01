0304caba3f6a8ba26d8ef13f6f59832ba5076702
felixAkerstromNTI2c <felix.akerstrom@elev.ga.ntig.se>
2025-04-01 22:49:22 +0200

    Nu går det att lägga till namn i klasser
    Namn kan läggas till i klasser, men kan inte raderas eller ändras ännu.

--- app.rb
+++ app.rb
@@ -5,6 +5,13 @@ require 'sinatra/reloader'
 require 'bcrypt'
 
 enable :sessions
+
+def get_db()
+  db = SQLite3::Database.new("db/database.db")
+  db.results_as_hash = true
+  return db
+end
+
 get('/') do
     slim(:start)
 end
@@ -18,19 +25,34 @@ get('/login') do
 end
 
 get('/classes') do
-  db = SQLite3::Database.new("db/database.db")
-	db.results_as_hash = true
+  db = get_db()
+	
 	groups = db.execute("SELECT * FROM 'group' WHERE userid = #{session[:id]}")
 
-    slim(:"classes/index", locals:{groups:groups})
+  slim(:"classes/index", locals:{groups:groups})
+end
+
+get('/classes/show/:id') do
+  group_id = params[:id]
+  db = get_db()
+  
+  members = db.execute("SELECT * FROM 'member_names' WHERE id IN (SELECT member_id FROM 'group_members' WHERE group_id = ?)", [group_id])
+  slim(:"classes/show", locals: {members: members, group_id: group_id})
 end
 
 get('/class_members') do
-  db = SQLite3::Database.new("db/database.db")
-  db.results_as_hash = true
-  members = db.execute("SELECT * FROM 'member_names'")
-
-  slim(:"class_members/index", locals:{members:members})
+  db = get_db()
+  
+  members = db.execute("
+    SELECT group.groupname, member_names.fullname
+    FROM group_members
+    INNER JOIN 'group' ON group_members.group_id = group.id
+    INNER JOIN member_names ON group_members.member_id = member_names.id
+    WHERE group.userid = ?", [session[:id]])
+  slim(:"class_members/index", locals: { members: members })
+  #members = db.execute("SELECT * FROM 'member_names'") # Realtionstabellen mellan grupper och medlemmar är inte implementerad i databasen än.
+  # members = db.execute("SELECT * FROM 'member_names' INNER JOIN 'group' ON member_names.id = group.id WHERE group.userid = #{session[:id]}")
+  # slim(:"class_members/index", locals:{members:members})
 end
 
 get('/classes/new') do
@@ -41,20 +63,38 @@ get('/classes/edit') do
   slim(:"classes/edit")
 end
 
-post('/classes/new') do
+get('/class_members/show/:id') do
+
+end
+
+post('/classes/create') do
 	userid = session[:id]
 	groupname = params[:groupname]
-	db = SQLite3::Database.new("db/database.db")
+	db = get_db()
 	db.execute("INSERT INTO 'group' (groupname, userid) VALUES (?,?)", [groupname,userid])
 	redirect('/classes')
 end
 
-post('/class_members/new') do
+get('/class_members/new') do
+  db = get_db()
+  
+  groups = db.execute("SELECT * FROM 'group' WHERE userid = #{session[:id]}")
+  slim(:"class_members/new", locals:{groups:groups})
+end
+
+post('/class_members/create') do
+  group_id = params[:group_id]
+  # member_id = params[:memberid]
   fullname = params[:fullname]
-  groupid = params[:groupid]
-  db = SQLite3::Database.new("db/database.db")
-  db.execute("INSERT INTO 'member_names' (fullname, groupid) VALUES (?,?)", [fullname, groupid])
-  redirect('/class_members')
+  # id = params[:groupid]
+  db = get_db()
+  db.execute("INSERT INTO 'member_names' (fullname) VALUES (?)", [fullname])
+  # Get the last inserted class_member_id
+  member_id = db.last_insert_row_id
+
+  # Insert the relationship into the junction table
+  db.execute("INSERT INTO group_members (group_id, member_id) VALUES (?, ?)", [group_id, member_id])
+  redirect("/classes/show/#{group_id}")
 end
 
 post('/login') do
@@ -101,6 +141,8 @@ post('/create_account') do
     "Lösenorden matchade inte! #{password} #{password_confirm}"
   end
 end
+
+
 
 get('/group/:id') do
 
