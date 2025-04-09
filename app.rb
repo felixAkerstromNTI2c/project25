79c600c3293b4a3a717f567ec3679903897fd3d8
felixAkerstromNTI2c <felix.akerstrom@elev.ga.ntig.se>
2025-04-09 23:06:46 +0200

    Påskinlämning

--- app.rb
+++ app.rb
@@ -5,22 +5,28 @@ require 'sinatra/reloader'
 require 'bcrypt'
 require_relative 'model/model.rb'
 also_reload 'model/model.rb'
+include Model
 
 enable :sessions
 
-
+# Displays the start page
 get('/') do
     slim(:start)
 end
 
+# Displays the account creation form
 get('/create_account') do
     slim(:register)
 end
 
+# Displays the login form
 get('/login') do
     slim(:login)
 end
 
+# Displays all classes that belong to the logged-in user
+#
+# @see Model#show_classes
 get('/classes') do
   session_id = session[:id]
 
@@ -28,31 +34,36 @@ get('/classes') do
   slim(:"classes/index", locals:{groups:groups})
 end
 
+# Displays members of a specific class
+#
+# @param [Integer] :id, the ID of the class
+# @see Model#show_class_members
 get('/classes/show/:id') do
   group_id = params[:id]
   members = show_class_members(group_id)
   slim(:"classes/show", locals: {members: members, group_id: group_id})
 end
 
+# Displays class members using an inner join with current user’s classes
+#
+# @see Model#inner_join_members
 get('/class_members') do
   session_id = session[:id]
   db = get_db()
   
   members = inner_join_members(session_id) 
   slim(:"class_members/index", locals: { members: members })
-  #members = db.execute("SELECT * FROM 'member_names'") # Realtionstabellen mellan grupper och medlemmar är inte implementerad i databasen än.
-  # members = db.execute("SELECT * FROM 'member_names' INNER JOIN 'group' ON member_names.id = group.id WHERE group.userid = #{session[:id]}")
-  # slim(:"class_members/index", locals:{members:members})
 end
 
+# Displays the form for creating a new class
 get('/classes/new') do
     slim(:"classes/new")
 end
 
-get('/classes/edit') do
-  slim(:"classes/edit")
-end
 
+# Handles creation of a new class
+#
+# @see Model#create_class
 post('/classes/create') do
 	userid = session[:id]
 	groupname = params[:groupname]
@@ -60,6 +71,9 @@ post('/classes/create') do
 	redirect('/classes')
 end
 
+# Displays form for adding a new class member
+#
+# @see Model#add_class_member_get
 get('/class_members/new') do
   db = get_db()
   session_id =session[:id]
@@ -67,6 +81,11 @@ get('/class_members/new') do
   slim(:"class_members/new", locals:{groups:groups})
 end
 
+# Handles creation of a new class member
+#
+# @param [Integer] :group_id, the ID of the group
+# @param [String] :fullname, the name of the new member
+# @see Model#add_class_member_post
 post('/class_members/create') do
   group_id = params[:group_id]
   fullname = params[:fullname]
@@ -74,6 +93,11 @@ post('/class_members/create') do
   redirect("/classes/show/#{group_id}")
 end
 
+# Deletes a class member
+#
+# @param [Integer] :id, the ID of the member
+# @param [Integer] :group_id, the ID of the group the member belongs to
+# @see Model#delete_class_member
 post('/class_members/:id/delete') do
   member_id = params[:id]
   group_id = params[:group_id] 
@@ -81,28 +105,35 @@ post('/class_members/:id/delete') do
   redirect("/classes/show/#{group_id}")
 end
 
+# Deletes a class
+#
+# @param [Integer] :id, the ID of the class
+# @see Model#delete_class
 post('/classes/:id/delete') do
   group_id = params[:id]
   delete_class(group_id)
   redirect('/classes')
 end
 
-
+# Handles user login
+#
+# @param [String] :username, the entered username
+# @param [String] :password, the entered password
+# @see Model#select_names
 post('/login') do
   username = params[:username]
   password = params[:password]
-  db = SQLite3::Database.new('db/database.db')
-  db.results_as_hash = true
+  db = get_db()
 
   session[:login_attempts] ||= 0
   session[:last_attempt_time] ||= Time.now
 
-  # Check if the user needs to wait before trying again
-  if session[:login_attempts] >= 3 && Time.now - session[:last_attempt_time] < 30
-    return "Too many failed attempts. Please wait #{30 - (Time.now - session[:last_attempt_time]).to_i} seconds before trying again."
+  
+  if session[:login_attempts] >= 2 && Time.now - session[:last_attempt_time] < 10
+    return "Too many failed attempts. Please wait #{10 - (Time.now - session[:last_attempt_time]).to_i} seconds before trying again."
   end
 
-  result = db.execute("SELECT * FROM users WHERE name = (?)", [username]).first
+  result = select_names(username) 
 
   if result.nil?
     session[:login_attempts] += 1
@@ -116,7 +147,7 @@ post('/login') do
       session[:id] = id
       session[:username] = username
       session[:adminlevel] = result["adminlevel"]
-      session[:login_attempts] = 0 # Reset attempts on successful login
+      session[:login_attempts] = 0
       redirect('/classes')
     else
       session[:login_attempts] += 1
@@ -126,82 +157,73 @@ post('/login') do
   end
 end
 
+# Logs the user out and clears the session
 get('/logout') do
   session.clear
   redirect('/')
 end
   
-
+# Handles account creation
+#
+# @param [String] :username, the desired username
+# @param [String] :password, the password
+# @param [String] :confirm_password, confirmation of the password
+# @see Model#new_account
+# @see Model#select_names
 post('/create_account') do
   adminlevel = 1
   name = params[:username]
   password = params[:password]
   password_confirm = params[:confirm_password]
 
-  db = SQLite3::Database.new('db/database.db')
-  db.results_as_hash = true
-  existing_user = db.execute("SELECT * FROM users WHERE name = ?", [name]).first
+  db = get_db()
+  existing_user = select_names(name)
 
   if existing_user
     "Username already exists!"
   elsif password == password_confirm
-    adminlevel = 2 if name == "admin"
+    adminlevel = 2 if name.include?("admin")
     password_digest = BCrypt::Password.create(password)
-    db.execute("INSERT INTO 'users' (name, pwdigest, adminlevel) VALUES (?, ?, ?)", [name, password_digest, adminlevel])
+    new_account(name, password_digest, adminlevel)
     redirect('/')
   else
     "Passwords do not match! #{password} #{password_confirm}"
   end
 end
 
+# Displays statistics about accounts, groups, and members
+#
+# Only accessible to admin users
+# @see Model#get_amount_of_users
+# @see Model#get_amount_of_groups
+# @see Model#get_amount_of_members
 get('/stats') do
   if session[:adminlevel] != 2
     redirect('/')
   end
-  account_count = get_amount_of_users() # db.execute("SELECT COUNT(*) AS count FROM users").first['count']
-  group_count = get_amount_of_groups() # db.execute("SELECT COUNT(*) AS count FROM 'group'").first['count']
-  member_count = get_amount_of_members() #db.execute("SELECT COUNT(*) AS count FROM member_names").first['count']
+  account_count = get_amount_of_users() 
+  group_count = get_amount_of_groups() 
+  member_count = get_amount_of_members() 
   slim(:stats, locals: { account_count: account_count, group_count: group_count, member_count: member_count })
 end
 
+# Picks a random member from a class and redisplays the class page
+#
+# @param [Integer] :id, the ID of the class
+# @see Model#select_members
 post('/classes/:id/random_name') do
   group_id = params[:id]
   db = get_db()
-  # Fetch all members of the class
+
   members = select_members(group_id) 
 
-  # Select a random member
   if members.any?
     random_member = members.sample['fullname']
   else
     random_member = 'No members in this class.'
   end
 
-  # Render the same page with the random name
   members = render_members(group_id)
   slim(:"classes/show", locals: { members: members, group_id: group_id, random_member: random_member })
 end
 
-
-
-
-# username = params["username"]
-# password = params["password"]
-# pasword_confirmation = params["confirm_password"]
-
-# result = db.execute("SELECT id FROM users WHERE name=?", name)
-
-# if result.empty?
-# if password == confirm_password
-#     pwdigest = BCrypt::password.create(password)
-#     p password_digest
-#     db.execute("INSERT INTO 'users' (username, pwdigest) VALUES (?,?)", [username, pwdigest])
-#     redirect("/")
-# else
-#     set_error("Passwords don't match")
-#     redirect("/error")
-# end
-# else
-# set_error("/Username alreaady exists")
-# redirect("/error")
-# end
--- model/model.rb
+++ model/model.rb
@@ -1,101 +1,171 @@
-# get db
-def get_db()
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
-    return db
-end
+module Model
 
-def show_classes(session_id)
-    db = get_db()
-    return db.execute("SELECT * FROM 'group' WHERE userid = #{session_id}")
-end
-
-def login_post(username, password)
-    return db.execute("SELECT * FROM users WHERE name = (?)", [username]).first
-end
-
-# users
-def add_class_member_get(session_id)
-    db.execute("SELECT * FROM 'group' WHERE userid = #{session_id}")
-end
-
-# classes
-def create_class(userid, groupname)
-    db = get_db()
-	db.execute("INSERT INTO 'group' (groupname, userid) VALUES (?,?)", [groupname,userid])
-end
-
-def show_class_members(group_id)
-    db = get_db()
-    
-    return db.execute("SELECT * FROM 'member_names' WHERE id IN (SELECT member_id FROM 'group_members' WHERE group_id = ?)", [group_id])
-
-
-end
-# members
-def add_class_member_post(group_id, fullname)
-  db = get_db()
-  db.execute("INSERT INTO 'member_names' (fullname) VALUES (?)", [fullname])
-  member_id = db.last_insert_row_id
-  db.execute("INSERT INTO group_members (group_id, member_id) VALUES (?, ?)", [group_id, member_id])
-end
-
-def delete_class_member(id)
-    db = get_db()
+    # Opens a connection to the SQLite3 database
+    #
+    # @return [SQLite3::Database] the database connection
+    def get_db()
+      db = SQLite3::Database.new("db/database.db")
+      db.results_as_hash = true
+      return db
+    end
   
-  # Delete the member from the group_members table
-  db.execute("DELETE FROM group_members WHERE member_id = ?", [id])
-end
-
-def delete_class(group_id)
-    db = get_db()
-
-  # Delete all relationships in the group_members table
-  db.execute("DELETE FROM group_members WHERE group_id = ?", [group_id])
-
-  # Optionally, delete all members associated with the group
-  db.execute("DELETE FROM member_names WHERE id IN (SELECT member_id FROM group_members WHERE group_id = ?)", [group_id])
-
-  # Delete the class itself
-  db.execute("DELETE FROM 'group' WHERE id = ?", [group_id])
-end
-
-def get_amount_of_users()
-    db = get_db()
-    user_count = db.execute("SELECT COUNT(*) AS count FROM users").first['count']
-    return user_count
-end
-
-def get_amount_of_groups()
-    db = get_db()
-    group_count = db.execute("SELECT COUNT(*) AS count FROM 'group'").first['count']
-    return group_count
-end
-
-def inner_join_members(session_id)
-    db = get_db()
-    # Perform an inner join to get group names and member names
-    return db.execute("
-    SELECT group.groupname, member_names.fullname
-    FROM group_members
-    INNER JOIN 'group' ON group_members.group_id = group.id
-    INNER JOIN member_names ON group_members.member_id = member_names.id
-    WHERE group.userid = ?", session_id)
-end
-
-
-def get_amount_of_members()
-    db = get_db()
-    member_count = db.execute("SELECT COUNT(*) AS count FROM member_names").first['count']
-    return member_count
-end
-
-def select_members(group_id)
-    db = get_db()
-    return db.execute("SELECT fullname FROM member_names WHERE id IN (SELECT member_id FROM group_members WHERE group_id = ?)", [group_id])
-end
-
-def render_members(group_id)
-    db = get_db()
-    return db.execute("SELECT * FROM member_names WHERE id IN (SELECT member_id FROM group_members WHERE group_id = ?)", [group_id])
-end
+    # Retrieves all classes for a given user
+    #
+    # @param [Integer] session_id, the ID of the logged-in user
+    # @return [Array<Hash>] list of classes
+    def show_classes(session_id)
+      db = get_db()
+      return db.execute("SELECT * FROM 'group' WHERE userid = #{session_id}")
+    end
+  
+    # Retrieves user data for login validation
+    #
+    # @param [String] username, the entered username
+    # @param [String] password, the entered password (unused here)
+    # @return [Hash, nil] user row or nil
+    def login_post(username, password)
+      return db.execute("SELECT * FROM users WHERE name = (?)", [username]).first
+    end
+  
+    # Retrieves all groups owned by the user
+    #
+    # @param [Integer] session_id, the ID of the logged-in user
+    # @return [Array<Hash>] list of groups
+    def add_class_member_get(session_id)
+      db = get_db()
+      db.execute("SELECT * FROM 'group' WHERE userid = #{session_id}")
+    end
+  
+    # Creates a new class
+    #
+    # @param [Integer] userid, the ID of the user creating the class
+    # @param [String] groupname, the name of the class
+    def create_class(userid, groupname)
+      db = get_db()
+      db.execute("INSERT INTO 'group' (groupname, userid) VALUES (?, ?)", [groupname, userid])
+    end
+  
+    # Retrieves all members of a specific class
+    #
+    # @param [Integer] group_id, the ID of the class
+    # @return [Array<Hash>] list of members
+    def show_class_members(group_id)
+      db = get_db()
+      return db.execute("SELECT * FROM 'member_names' WHERE id IN (SELECT member_id FROM 'group_members' WHERE group_id = ?)", [group_id])
+    end
+  
+    # Adds a new class member and links them to the group
+    #
+    # @param [Integer] group_id, the ID of the group
+    # @param [String] fullname, the full name of the new member
+    def add_class_member_post(group_id, fullname)
+      db = get_db()
+      db.execute("INSERT INTO 'member_names' (fullname) VALUES (?)", [fullname])
+      member_id = db.last_insert_row_id
+      db.execute("INSERT INTO group_members (group_id, member_id) VALUES (?, ?)", [group_id, member_id])
+    end
+  
+    # Deletes a class member from the group
+    #
+    # @param [Integer] id, the ID of the member to delete
+    def delete_class_member(id)
+      db = get_db()
+      db.execute("DELETE FROM group_members WHERE member_id = ?", [id])
+    end
+  
+    # Deletes a class and optionally its members
+    #
+    # @param [Integer] group_id, the ID of the class
+    def delete_class(group_id)
+      db = get_db()
+  
+      # Delete group-member relationships
+      db.execute("DELETE FROM group_members WHERE group_id = ?", [group_id])
+  
+      # Optionally delete members (not safe if they belong to other groups!)
+      db.execute("DELETE FROM member_names WHERE id IN (SELECT member_id FROM group_members WHERE group_id = ?)", [group_id])
+  
+      # Delete the class
+      db.execute("DELETE FROM 'group' WHERE id = ?", [group_id])
+    end
+  
+    # Gets the total number of users
+    #
+    # @return [Integer] number of users
+    def get_amount_of_users()
+      db = get_db()
+      user_count = db.execute("SELECT COUNT(*) AS count FROM users").first['count']
+      return user_count
+    end
+  
+    # Gets the total number of groups
+    #
+    # @return [Integer] number of groups
+    def get_amount_of_groups()
+      db = get_db()
+      group_count = db.execute("SELECT COUNT(*) AS count FROM 'group'").first['count']
+      return group_count
+    end
+  
+    # Gets the total number of members
+    #
+    # @return [Integer] number of members
+    def get_amount_of_members()
+      db = get_db()
+      member_count = db.execute("SELECT COUNT(*) AS count FROM member_names").first['count']
+      return member_count
+    end
+  
+    # Gets all members joined with their class names for a user
+    #
+    # @param [Integer] session_id, the ID of the user
+    # @return [Array<Hash>] list of member and class name pairs
+    def inner_join_members(session_id)
+      db = get_db()
+      return db.execute("
+        SELECT group.groupname, member_names.fullname
+        FROM group_members
+        INNER JOIN 'group' ON group_members.group_id = group.id
+        INNER JOIN member_names ON group_members.member_id = member_names.id
+        WHERE group.userid = ?", session_id)
+    end
+  
+    # Gets only the fullnames of members in a specific group
+    #
+    # @param [Integer] group_id, the ID of the group
+    # @return [Array<Hash>] list of member fullnames
+    def select_members(group_id)
+      db = get_db()
+      return db.execute("SELECT fullname FROM member_names WHERE id IN (SELECT member_id FROM group_members WHERE group_id = ?)", [group_id])
+    end
+  
+    # Gets full member rows for rendering a group
+    #
+    # @param [Integer] group_id, the ID of the group
+    # @return [Array<Hash>] member data
+    def render_members(group_id)
+      db = get_db()
+      return db.execute("SELECT * FROM member_names WHERE id IN (SELECT member_id FROM group_members WHERE group_id = ?)", [group_id])
+    end
+  
+    # Creates a new user account
+    #
+    # @param [String] name, the desired username
+    # @param [String] password_digest, the hashed password
+    # @param [Integer] adminlevel, the user's permission level
+    def new_account(name, password_digest, adminlevel)
+      db = get_db()
+      db.execute("INSERT INTO 'users' (name, pwdigest, adminlevel) VALUES (?, ?, ?)", [name, password_digest, adminlevel])
+    end
+  
+    # Looks up a user by username
+    #
+    # @param [String] name, the username
+    # @return [Hash, nil] user data or nil
+    def select_names(name)
+      db = get_db()
+      return db.execute("SELECT * FROM users WHERE name = ?", [name]).first
+    end
+  
+  end
+  
